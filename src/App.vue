aca804f93d2ff5dbe62491b0caa228e1318f66f5
GanGe <1074134594@qq.com>
2022-08-21 18:59:03 +0800

    项目的创建

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="btn">
+      <button>1</button>
+      <button>1</button>
+    </div>
   </div>
 </template>
 
